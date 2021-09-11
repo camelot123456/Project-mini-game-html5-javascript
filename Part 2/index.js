83bc642da39b15ae73dd17dc6e137449e7be3565
Bao Nguyen <nguyensybao1403.com>
2021-09-11 20:59:54 +0700

    update part 2

--- Part 2/index.js
+++ Part 2/index.js
@@ -4,8 +4,10 @@ var c = canvas.getContext('2d');
 canvas.width = 700;
 canvas.height = 500;
 
+var typeControl = false;
+
 let fps, fpsInterval, startTime, now, then, elapsed;
-var move = {x: 0, y: 0}
+
 var t = false;
 var posMouse = 0;
 
@@ -20,7 +22,8 @@ const player = {
     frameY: 0,
     hightJump: 10,
     speed: 3,
-    moving: false
+    moving: false,
+    angle: null
 };
 
 const playerSprite = new Image();
@@ -35,7 +38,7 @@ function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
 window.addEventListener('keydown', (e) => {
     keys[e.keyCode] = true;
     player.moving = true;
-    console.log(keys)
+    typeControl = true;
 });
 
 window.addEventListener('keyup', (e) => {
@@ -45,68 +48,107 @@ window.addEventListener('keyup', (e) => {
 
 function movePlayer() {
     var tMoving = 2;
-    if (keys[38] && player.y > 70) { // up
+    if (keys[38] && player.y > 70 || keys[87] && player.y > 70) { // up
         player.y -= player.speed;
         player.frameY = tMoving = 3;
     }
-    if (keys[40] && player.y + player.height + 70 < canvas.height) {// down
+    if (keys[40] && player.y + player.height + 70 < canvas.height
+        || keys[83] && player.y + player.height + 70 < canvas.height) {// down
         player.y += player.speed;
         player.frameY = tMoving = 0;
     }
-    if (keys[37] && player.x > 70) { // left
+    if (keys[37] && player.x > 70 || keys[65] && player.x > 70) { // left
         player.x -= player.speed;
         player.frameY = tMoving = 1;
     }
-    if (keys[39] && player.x + player.width + 70 < canvas.width) { // right
+    if (keys[39] && player.x + player.width + 70 < canvas.width
+        || keys[68] && player.x + player.width + 70 < canvas.width) { // right
         player.x += player.speed;
         player.frameY = tMoving = 2;
     }
-    if (keys[32]) { // right
-        for (let i = 0; i < player.hightJump; i++) {
-            if (player.y + 5 < player.hightJump && player.y - 5 > player.hightJump) {
-                player.hightJump -= player.hightJump;    
-            }
-            player.y -= i;
-        }
-        player.frameY = tMoving;
-    }
 }
 //------------------------------------------------------------------------------
+var move = { 
+    x: player.x + player.width / 2, 
+    y: player.y + player.height 
+}
 
-window.addEventListener('mousedown', (e)=>{
-    t = true;
-    move.x = e.x;
-    move.y = e.y;
+canvas.addEventListener('mousedown', (e) => {
+    move.x = e.x - canvas.offsetLeft;
+    move.y = e.y - canvas.offsetTop;
     player.moving = true;
+    typeControl = false
 })
 
-window.addEventListener('mouseup',(e)=>{
-    t = false;
-    player.moving = false;
+var _move = {
+    x: 0,
+    y: 0
+};
+
+canvas.addEventListener('mousemove', (e) => {
+    _move.x = e.x - canvas.offsetLeft;
+    _move.y = e.y - canvas.offsetTop;
 })
 
-function moveMouse(){
-    //console.log(player.x, player.y, move)
-    if (t == true) {
-        if(player.x + player.width / 2 < move.x){ // right
-            player.x += player.speed;
-            player.frameY = 2;
-        }
-        if(player.x + player.width / 2 > move.x){ // left
-            player.x -= player.speed;
-            player.frameY = 1;
-        }
-        if(player.y + player.height / 2 < move.y){ //}){ // up
-            player.y += player.speed;
-            player.frameY = 0;
-        }
-        if(player.y + player.height / 2 > move.y){ // down 
-            player.y -= player.speed;
-            player.frameY = 3;
-        }
-        
+function getDistance(obj, pos) {
+    return Math.floor(Math.hypot((obj.x + obj.width / 2) - pos.x, (obj.height + obj.y) - pos.y));
+}
+
+function getAngle(obj, mouse) {
+    return Math.atan2((obj.height + obj.y) - mouse.y, (obj.x + obj.width / 2) - mouse.x) * 180 / Math.PI;
+}
+
+function rotate(){
+    if (getAngle(player, _move) >= 65 && getAngle(player, _move) <= 115) {
+        player.frameY = 3;
+    }
+    if (getAngle(player, _move) >= -115 && getAngle(player, _move) <= -65) {
+        player.frameY = 0;
+    }
+    if ((getAngle(player, _move) >= 0 && getAngle(player, _move) <= 65)
+    || (getAngle(player, _move) >= -65 && getAngle(player, _move) <= -0.5)) {
+        player.frameY = 1;
+    }
+    if ((getAngle(player, _move) >= 115 && getAngle(player, _move) <= 180)
+    || (getAngle(player, _move) >= -179.5 && getAngle(player, _move) <= -115)) {
+        player.frameY = 2;
     }
 }
+
+function moveMouse() {
+
+    if (player.x + player.width / 2 < move.x) { // right
+        player.x += player.speed;
+        player.frameY = 2;
+    }
+    if (player.x + player.width / 2 > move.x) { // left
+        player.x -= player.speed;
+        player.frameY = 1;
+    }
+    if (player.y + player.height < move.y) { // up
+        player.y += player.speed;
+        player.frameY = 0;
+    }
+    if (player.y + player.height > move.y) { // down 
+        player.y -= player.speed;
+        player.frameY = 3;
+    }
+    
+    if (player.x + player.width / 2 < move.x && player.y + player.height < move.y
+        || player.x + player.width / 2 < move.x && player.y + player.height > move.y) {
+        player.frameY = 2;
+    }
+
+    if (player.x + player.width / 2 > move.x && player.y + player.height < move.y
+        || player.x + player.width / 2 > move.x && player.y + player.height > move.y) {
+        player.frameY = 1;
+    }
+    if (getDistance(player, move) <= 2.9) {
+        player.moving = false
+    }
+
+}
+
 
 //------------------------------------------------------------------------------
 
@@ -136,9 +178,15 @@ function animate() {
             player.width, player.height,
             player.x, player.y,
             player.width, player.height);
-        movePlayer();
-        moveMouse();
+        if (typeControl) {
+            movePlayer();
+        } else {
+            moveMouse();
+        }
+        rotate();
         handlerPlayerFrame();
+        getDistance(player, move)
+        
     }
 }
 
